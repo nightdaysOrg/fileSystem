1b373206dfe93a2d9dc36d3b859045a602dab120
niedongsheng <niedongsheng@doctorwork.com>
2017-10-09 14:29:59 +0800

    文件系统后端 文件上传

--- service/controllers.js
+++ service/controllers.js
@@ -1,29 +1,44 @@
+let fs = require('fs');
 let fileDealer = require('./main.js')
 
 
 let controllers = {
 
-    getFileList(req,res){
-        let param = {dir:'/'};
-        if(req.body.dir){
+    test(req, res) {
+        console.log(req.body.name);
+    },
+
+    getFileList(req, res) {
+        let param = { dir: '/' };
+        if (req.body.dir) {
             param.dir = req.body.dir;
         }
-        let menu = {children:[]};
-        fileDealer.getFileList(param.dir,menu);
-        if(menu){
+        let menu = { children: [] };
+        fileDealer.getFileList(param.dir, menu);
+        if (menu) {
             res.send(menu);
-        }else{
+        } else {
             res.send('没有数据');
         }
     },
 
-    uploadFile(req,res){
-        console.log(req.files);
+    uploadFile(req, res) {
+        // console.log(req.files);
+        // console.log(req.body);
+        let file = req.files[0];
+        let path = req.body.filePath;
+        fs.writeFile(path + file.originalname, file.buffer, { 'flag': 'w' }, function (error) {
+            if (error) {
+                throw error;
+            }
+            console.log('write success');
+        });
+        res.send('{"code":"success"}');
     }
 }
 
 
 module.exports = {
-    root : "fileSystem",
-    controllers : controllers
+    root: "fileSystem",
+    controllers: controllers
 }
